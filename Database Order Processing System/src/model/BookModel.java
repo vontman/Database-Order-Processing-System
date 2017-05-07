197444247f982e3048f427e96a551326e12f283e
vontman <vontman@gmail.com>
2017-05-07 17:26:03 +0200

    Added some functions in usermodel and bookmodel

--- Database Order Processing System/src/model/BookModel.java
+++ Database Order Processing System/src/model/BookModel.java
@@ -8,11 +8,10 @@ import java.util.Date;
 import java.util.List;
 
 import data.Book;
-import data.User;
 
 public class BookModel {
 	private static Model model = Model.instance;
-	public static void createBook(Book book){
+	public static boolean createBook(Book book){
 		List<String>cols = new ArrayList<String>();
 		List<String>vals= new ArrayList<String>();
 		cols.add("isbn");
@@ -23,12 +22,14 @@ public class BookModel {
 		try {
 			model.insert("BOOK", cols, vals);
 			System.out.println("Book added successfully.");
+			return true;
 		} catch (SQLException e) {
 			System.out.println("Book not added, Error : "+e.getMessage());
+			return false;
 		}
 	}
 	
-	public static void updateBook(Book book, String isbn){
+	public static boolean updateBook(Book book, String isbn){
 		List<String>cols = new ArrayList<String>();
 		List<String>vals= new ArrayList<String>();
 		cols.add("isbn");
@@ -44,24 +45,62 @@ public class BookModel {
 		try {
 			model.update("BOOK", cols, vals, "isbn", isbn);
 			System.out.println("Book updated successfully.");
+			return true;
 		} catch (SQLException e) {
 			System.out.println("Book not updated, Error : "+e.getMessage());
+			return false;
 		}
 	}
 	public static Book getBook(String isbn){
 
 		List<String>cols = new ArrayList<String>();
 		List<String>vals= new ArrayList<String>();
-		
+		Book book = new Book();
 		cols.add("isbn");
 		vals.add(isbn);
 		
 		try{
 			ResultSet result = model.select("BOOK", cols, vals);
-			
+			if(result.next()){
+				book.setProperty("isbn", result.getString("isbn"));	
+				book.setProperty("title", result.getString("title"));	
+				book.setProperty("price", ""+result.getInt("price"));	
+				book.setProperty("copies", ""+result.getInt("copies"));	
+				book.setProperty("publication_year", result.getString("publication_year"));	
+				book.setProperty("category_id", result.getString("category_id"));	
+				book.setProperty("publisher_name", result.getString("publisher_name"));	
+				book.setProperty("threshold", result.getString("threshold"));
+				book.setProperty("authors", getAuthors(isbn));
+				return book;
+			}else{
+				System.out.println("Book not found");
+				return null;
+			}
 		}catch(SQLException e){
 			System.out.println("ERROR finding the book :"+e.getMessage());
+			return null;
 		}
-		return null;
 	}
+	public static String getAuthors(String isbn){
+
+		List<String>cols = new ArrayList<String>();
+		List<String>vals= new ArrayList<String>();
+		StringBuilder builder = new StringBuilder();
+		cols.add("isbn");
+		vals.add(isbn);
+		
+		try{
+			ResultSet result = model.select("BOOK_HAS_AUTHORS", cols, vals);
+			while(result.next()){
+				if(builder.length() > 0)
+					builder.append(",");
+				builder.append(result.getString("authors_name"));
+			}
+			return builder.toString();
+		}catch(SQLException e){
+			System.out.println("ERROR finding the authors :"+e.getMessage());
+			return "";
+		}
+	}
+	
 }
--- Database Order Processing System/src/model/UserModel.java
+++ Database Order Processing System/src/model/UserModel.java
@@ -11,7 +11,7 @@ import data.User;
 
 public class UserModel {
 	private static Model model = Model.instance;
-	public static void createUser(User user){
+	public static boolean createUser(User user){
 		List<String>cols = new ArrayList<String>();
 		List<String>vals= new ArrayList<String>();
 		cols.add("firstname");
@@ -22,18 +22,22 @@ public class UserModel {
 		vals.add(user.getUserName());
 		cols.add("password");
 		vals.add(user.getPassword());
+		cols.add("manager");
+		vals.add("0");
 		cols.add("created");
 		vals.add(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
 		
 		try {
 			model.insert("USER", cols, vals);
 			System.out.println("User added successfully.");
+			return true;
 		} catch (SQLException e) {
 			System.out.println("User not added, Error : "+e.getMessage());
+			return false;
 		}
 	}
 	
-	public static void updateUser(User user, String userName){
+	public static boolean updateUser(User user, String userName){
 		List<String>cols = new ArrayList<String>();
 		List<String>vals= new ArrayList<String>();
 		cols.add("firstname");
@@ -48,8 +52,10 @@ public class UserModel {
 		try {
 			model.update("USER", cols, vals, "username", userName);
 			System.out.println("User updated successfully.");
+			return true;
 		} catch (SQLException e) {
 			System.out.println("User not updated, Error : "+e.getMessage());
+			return false;
 		}
 	}
 	public static User getUser(String userName){
@@ -59,12 +65,21 @@ public class UserModel {
 		
 		cols.add("username");
 		vals.add(userName);
-		
+		User user = new User();
 		try{
 			ResultSet result = model.select("USER", cols, vals);
-			
+			if(result.next()){
+				user.setProperty("username", result.getString("username"));
+				user.setProperty("firstname", result.getString("firstname"));
+				user.setProperty("lastname", result.getString("lastname"));
+				user.setProperty("password", result.getString("password"));
+				return user;
+			}else{
+				System.out.println("User not found");
+				return null;
+			}
 		}catch(SQLException e){
-			System.out.println("ERROR finding user :"+e.getMessage());
+			System.out.println("ERROR when finding user :"+e.getMessage());
 		}
 		return null;
 	}
