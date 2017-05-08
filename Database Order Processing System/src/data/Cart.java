9152e8cb2af5e2859a0d1ca10ee1257e03df0fcf
fathy <muhammed.fat7y@gmail.com>
2017-05-09 01:54:41 +0300

    Cart methods are added

--- Database Order Processing System/src/data/Cart.java
+++ Database Order Processing System/src/data/Cart.java
@@ -1,7 +1,31 @@
 package data;
 
-import java.util.Properties;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
-public class Cart extends Properties{
+import model.BookModel;
+
+public class Cart {
+	private HashMap<String, Integer> books;
 	
+	public Cart() {
+		books = new HashMap<>();
+	}
+	
+	public void addBook(String isbn, int copies) {
+		books.put(isbn, books.getOrDefault(isbn, 0) + copies);
+	}
+	
+	public void removeBook(String isbn) {
+		books.remove(isbn);
+	}
+	
+	public int getCost() throws SQLException {
+		int ret = 0;
+		for (Entry<String, Integer> book : books.entrySet()) {
+			ret += BookModel.getBook(book.getKey()).getPrice() * book.getValue();
+		}
+		return ret;
+	}
 }
